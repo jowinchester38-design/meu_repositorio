0b4da4e17e51e3b3d33f1e3d590cad9ccde9603e
jowinchester38-design <jowinchester38@gmail.com>
2025-08-28 18:56:16 -0300

    Biscoito

---
+++ scripts.js
@@ -0,0 +1,13 @@
+/* mensagem e números aleatórios*/
+const mensagemAleatoria = mensagens[Math.floor(Math.random() * mensagens.length)];
+const numerosAleatorios = numerosDaSorte[Math.floor(Math.random() * numerosDaSorte.length)];
+
+// Exibe na página
+document.getElementById('mensagem').textContent = mensagemAleatoria;
+document.getElementById('numeros-sorte').textContent = numerosAleatorios;
+
+document.addEventListener('DOMContentLoaded', function() {
+
+});
+
+
